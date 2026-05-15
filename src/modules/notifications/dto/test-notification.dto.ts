dcf2b1e7e4de970ed2f188fe59a163f3b22557a2
kong mong thao <kongmongthao348@gmail.com>
2026-05-15 17:43:41 +0700

    udpate codeddd

--- src/modules/notifications/dto/test-notification.dto.ts
+++ src/modules/notifications/dto/test-notification.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional } from 'class-validator';
 
 export class TestNotificationDto {
   @ApiProperty({ 
@@ -6,6 +7,7 @@ export class TestNotificationDto {
     example: '1004039',
     required: true 
   })
+  @IsString()
   vbCode: string;
 
   @ApiProperty({ 
@@ -13,5 +15,7 @@ export class TestNotificationDto {
     example: 'ທົດສອບການແຈ້ງເຕືອນ',
     required: false 
   })
+  @IsOptional()
+  @IsString()
   message?: string;
 }
